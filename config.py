ed7d47176d1b7a3a49dbbf219b7c12adb35bbd1d
javerauoc <javera@uoc.edu>
2025-04-08 22:35:29 +0200

    refactoring

--- config.py
+++
@@ -1,9 +0,0 @@
-
-
-config: dict[str, str]=\
-    {
-    "url_archive": "https://archive.org/details/librivoxaudio?tab=collection",
-    "arxiu_sortida_dataset":"ds_audiollibres_archive.csv",
-    "url_llista_proxies":"https://proxy.webshare.io/api/v2/proxy/list/download/fwqctsfkaejzanaulnxkrjhjwwmdklcpsbxskwiw/-/any/username/direct/-/",
-    "robots_archive":""
-}
